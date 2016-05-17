f7624b2870cd28f9d98f450a3a8f78972e399b89
tomiviljanen <tomiviljanen@yahoo.fi>
2016-05-17 10:21:50 +0300

    Form
    Form connects mysql

---
+++ createuser.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Create user</title>
+
+</head>
+
+<body>
+
+<?php //lisaasql.php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+if (empty($username) || empty ($password) || empty ($email)) { 
+echo "<h3>Alueet eiv&aumlt saa olla tyhjiä</h3>";
+echo "<a href=\"index.php\">Takaisin</a>";} else {
+//Otetaan yhteys tietokantapalvelimeen
+$yhteys = mysql_connect('localhost', 'root', 'cnfCa8hb');
+// Valitaan oikea tietokanta
+mysql_select_db('users', $yhteys);
+// Tallennetaan uusi viesti tietokantaan SQL-lauseella
+mysql_query("INSERT INTO Auto (username, malli, vuosimalli, ajokilometrit, vari, hinta) VALUES ('$merkki', '$malli', '$vuosimalli', '$ajokilometrit', '$vari', '$hinta')");
+// Katkaistaan yhteys
+mysql_close($yhteys);
+header("Location: autot.php"); 
+}
+?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,17 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title></title>
+</head>
+<body>
+
+<form id="create user" method="post" action="createuser.php">
+
+
+<label for="username">Koko nimi</label>
+<input type="text" name="username" id="username" placeholder="Esim. Matti Matikainen"/>
+
+<label for="password">Salasana</label>
+<input type="password" name="password" id="password"/>
+
+<label for="email">Sähköposti</label>
+<input type="email" name="email" id="email" placeholder="Esim. matti@gmail.com"/>
+
+<input type="Submit" value="Laheta">
+</form>
 <?php
+// vieraskirjasql.php
 
+//Yhteys
+$yhteys = mysql_connect('localhost', 'root', 'cnfCa8hb');
 
+//Tietokanta
+mysql_select_db('users', $yhteys);
 
+//SQL-kysely
+$result = mysql_query('SELECT name, pass, sahkoposti, admin, user_id FROM users ORDER BY name ASC', $yhteys);
 
+//Rivimäärä
+$lkm = mysql_num_rows($result);
 
+//Tulokset läpi
+$i = 1;
+while($i <= $lkm) {
+	
+	$rivi = mysql_fetch_assoc($result);
+	
+	echo "<p>" . $rivi['name'] . " " . $rivi['pass'] . " (" . $rivi['sahkoposti'] . ")";
+	echo "<br>Onko admin " . $rivi['admin'] . ", User id <b>" . $rivi['user_id'] . "€</b>";
+	$i++;
+}
 
-
-
-
-
-
-
-
-
-
+mysql_close($yhteys);
 ?>
+</body>
+</html>
