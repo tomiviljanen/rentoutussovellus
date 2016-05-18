28ea0b17d30ca41692efd725560d8908e6bc069b
tomiviljanen <tomiviljanen@yahoo.fi>
2016-05-18 10:00:04 +0300

    Login stuff
    Changed index.php to login php so the user will log in from seperate
    site and then get redirected to index.php. If user is not logged in he
    will get redirected back to login.php. Now stores user data in session .
    old login verification was login.php new one is loginverify.php

--- index.php
+++ index.php
@@ -1,58 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title></title>
-</head>
-<body>
-<style>
-body { margin:0; padding:0; }
-div{
-	background-color: #A50E2D;
-	color: #dbdc2b;
-	display: inline-block;
-	min-width: 320px;
-	text-align: center;
-}
+<?php
+session_start();
+echo "Hello " . $_SESSION["firstname"] . $_SESSION["surname"]
 
-input[type="submit"]{
-	margin-top: 25px;
-}
 
-</style>
-<div>
-	<!-- Kirjautuiminen sisään-->
-	<form id="log in" method="post" action="login.php">
-		<h2>Kirjaudu sisään</h2>
-		<label for="email">Sähköposti</label><br>
-		<input type="email" name="email" id="email"/>
 
-		<br>
-		<label for="password">Salasana</label><br>
-		<input type="password" name="password" id="password"/>
-		
-		<br>
-		<input type="submit" value="Lähetä">
-	</form>
-</div>
-<div>
-	<!-- Uuden käyttäjän teko-->
-	<form id="create user" method="post" action="createuser.php">
-		<h2>Tee uusi käyttäjä</h2>
-		<label for="username">Koko nimi</label><br>
-		<input type="text" name="username" id="username"/>
-		
-		<br>
-		<label for="password">Salasana</label><br>
-		<input type="password" name="password" id="password"/>
-		
-		<br>
-		<label for="email">Sähköposti</label><br>
-		<input type="email" name="email" id="email"/>
 
-		<br>
-		<input type="Submit" value="Laheta">
-	</form>
-</div>
-</body>
-</html>
+
+
+
+
+?>
